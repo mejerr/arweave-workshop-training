95f0e2ddd9134b1002115cd03874e69233b6422f
Georgi Radev <georgiradev@Georgis-MacBook-Pro-3.local>
2022-07-29 10:33:46 +0300

    Added functionality for uploading a video on arweave

--- arweave-app/pages/_app.js
+++ arweave-app/pages/_app.js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   const [bundlrInstance, setBundlrInstance] = useState();
   const [balance, setBalance] = useState(0);
-  
+
   // set the base currency as matic (this can be changed later in the app)
   const [currency, setCurrency] = useState('matic');
   const bundlrRef = useRef();
@@ -17,13 +17,13 @@ function MyApp({ Component, pageProps }) {
   // create a function to connect to bundlr network
   async function initialiseBundlr() {
     await window.ethereum.enable();
-  
+
     const provider = new providers.Web3Provider(window.ethereum);
     await provider._ready();
-  
+
     const bundlr = new WebBundlr("https://node1.bundlr.network", currency, provider);
     await bundlr.ready();
-    
+
     setBundlrInstance(bundlr);
     bundlrRef.current = bundlr;
     fetchBalance();
@@ -81,19 +81,19 @@ const navStyle = css`
   border-bottom: 1px solid #ededed;
   display: flex;
   align-items: center;
-`
+`;
 
 const homeLinkStyle = css`
   display: flex;
   flex-direction: row;
   align-items: center;
-`
+`;
 
 const homeLinkTextStyle = css`
   font-weight: 200;
   font-size: 28;
   letter-spacing: 7px;
-`
+`;
 
 const footerStyle = css`
   border-top: 1px solid #ededed;
@@ -105,13 +105,13 @@ const footerStyle = css`
   font-weight: 200;
   letter-spacing: 1px;
   font-size: 14px;
-`
+`;
 
 const containerStyle = css`
   min-height: calc(100vh - ${navHeight + footerHeight}px);
   width: 900px;
   margin: 0 auto;
   padding: 40px;
-`
+`;
 
-export default MyApp
+export default MyApp;
---
+++ arweave-app/utils.js
@@ -0,0 +1,5 @@
+import Arweave from 'arweave';
+
+export const arweave = Arweave.init({});
+
+export const APP_NAME = 'MY_ARWEAVE_APP';
